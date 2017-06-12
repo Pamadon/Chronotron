88b1129975ac6c645a27183fbb4b7dca2f3f7c06
sdkevorkian <sdkevorkian@gmail.com>
2017-06-12 13:47:22 -0700

    moving some things around

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -1,7 +1,7 @@
 class MusicController < ApplicationController
   def show
 
-
+    @user = RSpotify::User.new(request.env['omniauth.auth'])
     # when we need to have genres available
     wanted_playlist_time_in_min = 30 * 60000
     # @genres = RSpotify::Recommendations.available_genre_seeds
@@ -23,8 +23,5 @@ class MusicController < ApplicationController
     @playlist = your_playlist
   end
 
-  def spotify_user
-    @user = RSpotify::User.new(request.env['omniauth.auth'])
 
-  end
 end
