afe8f9e7d367a4cd742e01a52c5f0c8e6137c6d1
sdkevorkian <sdkevorkian@gmail.com>
2017-06-12 13:39:44 -0700

    testing spotify login

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   get 'music' => 'music#show'
 
-  get '/auth/spotify/callback', to: 'music#spotifyuser'
+  get '/auth/spotify/callback', to: 'music#spotify_user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
