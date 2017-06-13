df8530e999bb915aa19a5ca9971d6552be2b8982
Peter Amadon <peteramadon@Peters-MacBook-Pro.local>
2017-06-13 11:05:57 -0700

    working on programtic creation of iframe

--- app/controllers/you_tube_controller_controller.rb
+++ app/controllers/you_tube_controller_controller.rb
@@ -1,49 +1,36 @@
 class YouTubeControllerController < ApplicationController
 
   def index
-
-
   end
-
 
   def show
-
-    videos = Yt::Collections::Videos.new
-    puts videos.inspect
-    puts videos.where()
-
- # 	response = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet&q=lady+-gaga&key=AIzaSyB9vzAQ4Nn-Ig3fzaRdDEO0zptnI85vPSM'", {
-	# 	query: {term: params['search']['query']}
-	# })
-	# 	@data = nil
-	# 	if response.code == 200
-	# 		@data = JSON.parse(response.body)
- #  end
   end
+
   def search
     @keyWord = params[:keyWord]
+    @time = params[:time]
+    @numberVideos = params[:numberVideos]
     puts @keyWord
     response = HTTParty.get('https://www.googleapis.com/youtube/v3/search?', {
       query: {
-         part: 'snippet',
-          maxResults: '3',
+          part: 'snippet',
+          maxResults: @numberVideos,
           order: 'rating',
           q: @keyWord,
           type: 'video',
-          videoDuration: 'short',
+          videoDuration: @time,
           videoEmbeddable: 'true',
           key: 'AIzaSyB9vzAQ4Nn-Ig3fzaRdDEO0zptnI85vPSM'
           }
   })
-  puts response.code
-
-  if response.code == 200
-    @video_result = JSON.parse(response.body)
-    for item in  @video_result['items']
-      puts @video_result['items'][1]
-    end
-  end
-
+    $videos = []
+    if response.code == 200
+      @video_result = JSON.parse(response.body)
+        for item in  @video_result['items']
+        $videos.push(item['id']['videoId'])
+        end
+     end
+    puts $videos[1]
 
     @data = response['items'][0]['id']['videoId']
     @data1 = response['items'][1]['id']['videoId']
