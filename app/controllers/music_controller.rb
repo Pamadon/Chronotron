f3dd89e9847b68e7d4873628d2b1e20ff1cbcb81
sdkevorkian <sdkevorkian@gmail.com>
2017-06-13 11:23:04 -0700

    time is grabable from music controller

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -29,7 +29,8 @@ class MusicController < ApplicationController
   def show
     @user = RSpotify::User.new(session[:spotify_hash])
     # when we need to have genres available
-    wanted_playlist_time_in_ms = 30 * 60000
+    time = $time.to_i
+    wanted_playlist_time_in_ms = time * 60000
     # @genres = RSpotify::Recommendations.available_genre_seeds
     result = RSpotify::Recommendations.generate(limit: 20, seed_genres: ['dubstep'])
     tracks = result.tracks
@@ -45,7 +46,6 @@ class MusicController < ApplicationController
       your_playlist.push(current_track)
       track_counter += 1
       current_playlist_time -= current_track.duration_ms
-      puts current_playlist_time
     end
 
     @playlist.add_tracks!(your_playlist)
