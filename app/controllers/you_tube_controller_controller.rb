4aa9c70d9da6300e193a02cd719b94c61520c626
Peter Amadon <peteramadon@Peters-MacBook-Pro.local>
2017-06-12 17:06:38 -0700

    Added ability to drop video Id's into iframes

--- app/controllers/you_tube_controller_controller.rb
+++ app/controllers/you_tube_controller_controller.rb
@@ -8,10 +8,9 @@ class YouTubeControllerController < ApplicationController
 
   def show
 
-
- videos = Yt::Collections::Videos.new
- puts videos.where(order: 'viewCount').first.title
- puts videos.where()
+    videos = Yt::Collections::Videos.new
+    puts videos.inspect
+    puts videos.where()
 
  # 	response = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet&q=lady+-gaga&key=AIzaSyB9vzAQ4Nn-Ig3fzaRdDEO0zptnI85vPSM'", {
 	# 	query: {term: params['search']['query']}
@@ -20,16 +19,25 @@ class YouTubeControllerController < ApplicationController
 	# 	if response.code == 200
 	# 		@data = JSON.parse(response.body)
  #  end
-end
-  def search
-  @keyWord = params[:keyWord]
-  puts @keyWord
-  response = HTTParty.get('https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyB9vzAQ4Nn-Ig3fzaRdDEO0zptnI85vPSM', {
-
-  })
-  puts response
   end
+  def search
+    @keyWord = params[:keyWord]
+    puts @keyWord
+    response = HTTParty.get('https://www.googleapis.com/youtube/v3/search?', {
+      query: {
+         part: 'snippet',
+          maxResults: '3',
+          order: 'rating',
+          q: @keyWord,
+          type: 'video',
+          videoDuration: 'short',
+          videoEmbeddable: 'true',
+          key: 'AIzaSyB9vzAQ4Nn-Ig3fzaRdDEO0zptnI85vPSM'
+          }
+  })
+    @data = response['items'][0]['id']['videoId']
 
+  end
 
 end
 
