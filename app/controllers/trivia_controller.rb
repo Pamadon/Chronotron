61d75d66587e2fc14b40d463a84ad0a7e28218ef
jigglor <andrewtsuji@gmail.com>
2017-06-13 11:07:06 -0700

    working on converting special charaters

--- app/controllers/trivia_controller.rb
+++ app/controllers/trivia_controller.rb
@@ -6,6 +6,6 @@ class TriviaController < ApplicationController
 	def show
 		response = HTTParty.get('https://opentdb.com/api.php?amount=1&category=15&type=boolean')
 
-		@trivia = response["results"][0]["question"].to_s
+		@trivia = response["results"][0]["question"]
 	end
 end
