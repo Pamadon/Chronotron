3e0431c1a30b63fa89de1030cccda6b055dfe7d2
Peter Amadon <peteramadon@Peters-MacBook-Pro.local>
2017-06-13 09:01:03 -0700

    uncommented a commented line in application.rb that allowed spotify to work

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Chronotron
   class Application < Rails::Application
 
 
-    # RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
+    RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
