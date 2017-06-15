e78c0c92f75b514dca78247aed0284ccec1f5616
sdkevorkian <sdkevorkian@gmail.com>
2017-06-15 12:55:12 -0700

    button texts populated, can click and change current category

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,34 +1,46 @@
+var currentCategory = gon.choice;
 
 
-// function getCurrentCategory(){
-//   return $("#currentCategory")[0].innerText;
-// }
+function otherCatgoriesAvailable(currentCategory){
+    var otherCategories = [];
+    switch (currentCategory){
+      case "videos":
+        otherCategories = ["trivia", "music", "trails"];
+        break;
+      case "trivia":
+        otherCategories = ["videos", "music", "trails"];
+        break;
+      case "music":
+        otherCategories = ["videos", "trivia", "trails"];
+        break;
+      case "trails":
+        otherCategories = ["videos", "trivia", "music"];
+        break;
+    }
+    return otherCategories;
+}
 
-// function otherCatgoriesAvailable(currentCategory){
-//     var otherCategories = [];
-//     switch (currentCategory){
-//       case "videos":
-//         otherCategories = ["trivia", "music", "trails"];
-//         break;
-//       case "trivia":
-//         otherCategories = ["videos", "music", "trails"];
-//         break;
-//       case "music":
-//         otherCategories = ["videos", "trivia", "trails"];
-//         break;
-//       case "trails":
-//         otherCategories = ["videos", "trivia", "music"];
-//         break;
-//     }
-//     return otherCategories;
-// }
 
-// function showOtherCategories()
+function toggleCategory(){
+  currentCategory = this.innerText;
+  console.log(currentCategory);
+}
 
 $(document).ready(function(){
-    // var currentCategory = (getCurrentCategory());
 
-    // var otherCategories = otherCatgoriesAvailable(currentCategory);
-    console.log("test");
-    console.log(gon);
+
+    var otherCategories = otherCatgoriesAvailable(currentCategory);
+
+    var buttons = $('.choice-buttons');
+
+    buttons.on("click", toggleCategory);
+
+
+
+
+    for (var i=0; i < buttons.length; i++){
+      buttons[i].innerText = otherCategories[i];
+      console.log(buttons[i]);
+    }
+
 });
