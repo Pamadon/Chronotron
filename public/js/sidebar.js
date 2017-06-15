f4339b28bc199e83b00f6ba52c8c080571d74e2e
sdkevorkian <sdkevorkian@gmail.com>
2017-06-15 13:28:15 -0700

    buttons change when category switched

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,46 +1,55 @@
 var currentCategory = gon.choice;
 var buttons;
+var otherCategories;
 
-function otherCatgoriesAvailable(currentCategory){
+function otherCatgoriesAvailable(currentCategory) {
     var otherCategories = [];
-    switch (currentCategory){
-      case "videos":
-        otherCategories = ["trivia", "music", "trails"];
-        break;
-      case "trivia":
-        otherCategories = ["videos", "music", "trails"];
-        break;
-      case "music":
-        otherCategories = ["videos", "trivia", "trails"];
-        break;
-      case "trails":
-        otherCategories = ["videos", "trivia", "music"];
-        break;
+    switch (currentCategory) {
+        case "videos":
+            otherCategories = ["trivia", "music", "trails"];
+            break;
+        case "trivia":
+            otherCategories = ["videos", "music", "trails"];
+            break;
+        case "music":
+            otherCategories = ["videos", "trivia", "trails"];
+            break;
+        case "trails":
+            otherCategories = ["videos", "trivia", "music"];
+            break;
     }
     return otherCategories;
 }
 
-function displayCategoryForms(category){
-  $(`#${category}`).show();
-  $(`#${category}`).siblings().hide();
+function displayCategoryForms(category) {
+    $(`#${category}`).show();
+    $(`#${category}`).siblings().hide();
 }
 
 
-function toggleCategory(){
-  currentCategory = this.innerText;
-  for (var i = 0; i < buttons.length; i++) {
-    if (buttons[i].innerText ===currentCategory){
-      displayCategoryForms(currentCategory);
+function toggleCategory() {
+    currentCategory = this.innerText;
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttons[i].innerText === currentCategory) {
+            displayCategoryForms(currentCategory);
+        }
     }
-  }
+    changeButtonText(currentCategory);
 }
 
-$(document).ready(function(){
-    var otherCategories = otherCatgoriesAvailable(currentCategory);
+function changeButtonText() {
+    otherCategories = otherCatgoriesAvailable(currentCategory);
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].innerText = otherCategories[i];
+    }
+}
+
+$(document).ready(function() {
+    otherCategories = otherCatgoriesAvailable(currentCategory);
 
     buttons = $('.choice-buttons');
-    for (var i=0; i < buttons.length; i++){
-      buttons[i].innerText = otherCategories[i];
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].innerText = otherCategories[i];
     }
     displayCategoryForms(currentCategory);
 
